0fae2fbe7d426845eeb4509f2013d4353ffdb45b
fcdev <fedechico1@live.it>
2020-04-16 17:59:22 +0200

    added error message to the InvalidSimulatorException

--- src/com/epidemic_simulator/InvalidSimulationException.java
+++ src/com/epidemic_simulator/InvalidSimulationException.java
@@ -1,4 +1,14 @@
 package com.epidemic_simulator;
 
 public class InvalidSimulationException extends Exception{
+    private String message;
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    public InvalidSimulationException(String message) {
+        this.message = message;
+    }
 }
