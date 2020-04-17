fa184ed0aeb2680570362f657e07d68009beb175
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-04-17 18:10:10 +0200

    by Paoletto: added comments note: i had to rebase it since your commits did strange things (it ignored the gitignore and had a strange behaviour with the difftool)

--- src/com/epidemic_simulator/Person.java
+++ src/com/epidemic_simulator/Person.java
@@ -11,46 +11,48 @@ public class Person {
     protected boolean immune       = false;
 
     //disease evolution
-    protected int daysSinceInfection = 0;
-    protected int symptomsDevelopmentDay =-1;
-    protected int deathDay =-1;
+    protected int daysSinceInfection = 0; //Numero giorni passati dal contagio.
+    protected int symptomsDevelopmentDay =-1; //Numero giorni entro cui eventualmente presenterà sintomi,-1 dato che la persona potrebbe non svilupparli.
+    protected int deathDay =-1; //Numero giorni entro cui eventualmente la persona in questione morirà,-1 dato che la persona potrebbe anche non morire.
     //#endregion
 
-    protected boolean canMove = true;
+    protected boolean canMove = true; //All'inizio tutti gli individui possono muoversi
 
     public Color getColor(){
-        //if he's not alive then he's dead (Captain obvious to the rescue!)
+        //Se l'individuo ha status-alive=false è sicuramente nero/morto.
         if(!alive) return Color.BLACK;
         //blu un individuo guarito
         if(immune) return Color.BLUE;
 
-        //verde, che rappresenta un individuo sano o con carica batterica non rilevabile e comunque non contagioso
+        //verde, che rappresenta un individuo sano o con carica batterica non rilevabile e comunque non contagioso con CanInfect=false
         if(!canInfect) return Color.GREEN;
-        //giallo, che indica un individuo contagioso ma asintomatico
+        //giallo, che indica un individuo contagioso ma asintomatico con symptoms=false
         if(!symptoms) return Color.YELLOW;
         //rosso un individuo sintomatico
         return Color.RED;
     }
 
+    //Una volta avvenuto un'incontro tra un individuo sano e uno infetto in movimento,la seguente funzione verrà richiamata per vedere se il soggetto sano può essere stato infetto o meno.
     protected void tryInfect(int infectionRate, int symptomsRate, int deathRate, int incubation, int maxDayForSymptomsDevelopment, int healDay){
-        if(immune) return;
+        if(immune) return;//Se il soggetto è immune usciamo subito...
 
-        //check infectivity
-        if(Utils.randomBool(infectionRate)){
-            infect(symptomsRate, deathRate, incubation, maxDayForSymptomsDevelopment, healDay);
+        //check sull'infettività
+        if(Utils.randomBool(infectionRate)){//Data la percentuale di infezione(25% ad esempio)Util.randomBool ritornerà true se rientra in quella percentuale di infezione.
+            infect(symptomsRate, deathRate, incubation, maxDayForSymptomsDevelopment, healDay);//Se entriamo nell'if abbiamo un'infezione avvenuta
         }
     }
 
+    //Una volta che l'infezione è avvenuta verifichiamo con la funzione come il soggetto reagirà alla malattia.
     protected void infect(int symptomsRate, int deathRate, int incubation, int maxDayForSymptomsDevelopment, int healDay) {
-        infected = true;
+        infected = true;//La persona passerà a questo punto il suo attributo 'infected' a true.
 
         //check symptomaticity
-        if(!Utils.randomBool(symptomsRate)) return;
-        symptomsDevelopmentDay = Utils.random(incubation, maxDayForSymptomsDevelopment);
-
-        //check death
+        if(!Utils.randomBool(symptomsRate)) return; /*Se il soggetto non rientra nel range di sintomaticità(anche questa in percentuale)
+        allora resterà giallo fino alla sua guarigione ed usciamo...*/
+        symptomsDevelopmentDay = Utils.random(incubation, maxDayForSymptomsDevelopment);//Altrimenti calcoliamo un giorno randomico entro cui una persona può presentare sintomi
+        //check death-->Controlliamo con lo stesso principio se il soggetto rientra nella percentuale di letalità.
         if(!Utils.randomBool(deathRate)) return;
-        deathDay = Utils.random(symptomsDevelopmentDay, healDay-1);
+        deathDay = Utils.random(symptomsDevelopmentDay, healDay-1);//Calcolo di un giorno entro cui la persona morirà.
     }
 
     public void setCanMove(boolean canMove) {
