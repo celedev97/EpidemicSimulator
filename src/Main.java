295734aa4ddee160b51fa0a6dbec8e9e98d703ca
Bruner4 <brunerfour@gmail.com>
2020-04-25 00:32:26 +0200

    Fixed last day report and added the elapsed time in milliseconds

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import com.epidemic_simulator.InvalidSimulationException;
 import com.epidemic_simulator.Simulator;
 import com.epidemic_simulator.Strategy;
-import strategies.PreemptiveLockdownAndStopSpread;
+import strategies.*;
 
 import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args){
+
+        long start = System.currentTimeMillis();
+
         Simulator sim = null;
 
         try {
@@ -24,7 +27,6 @@ public class Main {
         while ((outcome = sim.executeDay())== Simulator.Outcomes.NOTHING){
             System.out.println();
             System.out.println("DAY "+sim.getDay());
-
             int black   = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLACK).count();
             int green   = (int) sim.population.stream().filter(person -> person.getColor() == Color.GREEN).count();
             int yellow  = (int) sim.population.stream().filter(person -> person.getColor() == Color.YELLOW).count();
@@ -38,6 +40,28 @@ public class Main {
             System.out.println("black : "+black );
             System.out.println("resources : "+sim.getResources());
         }
+        System.out.println("\nFinal report:");
         System.out.println(sim.getDay() +"d "+ outcome.toString());
+
+        int black   = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLACK).count();
+        int green   = (int) sim.population.stream().filter(person -> person.getColor() == Color.GREEN).count();
+        int yellow  = (int) sim.population.stream().filter(person -> person.getColor() == Color.YELLOW).count();
+        int red     = (int) sim.population.stream().filter(person -> person.getColor() == Color.RED).count();
+        int blue    = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLUE).count();
+//        int R0 = (sim.averageEncountersPerDay * sim.healDay * sim.infectionRate)/100;
+        //TODO: dear Cencia, is this R0 useful??
+
+        System.out.println("green : "+green );
+        System.out.println("yellow: "+yellow);
+        System.out.println("red   : "+red   );
+        System.out.println("blue  : "+blue  );
+        System.out.println("black : "+black );
+        System.out.println("resources : "+sim.getResources());
+//        System.out.println("Fattore R0: " + R0);
+
+        long end = System.currentTimeMillis();
+        long time=(end-start);
+        System.out.println();
+        System.out.println(time + " millisec");
     }
 }
