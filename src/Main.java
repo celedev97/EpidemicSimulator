8cd4f47f787de2af2a170a9a89ae3e597174224a
Bruner4 <brunerfour@gmail.com>
2020-04-29 02:17:06 +0200

    Fixes reverted in Main and Simulator

--- src/Main.java
+++ src/Main.java
@@ -7,61 +7,60 @@ import java.awt.*;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         long start = System.currentTimeMillis();
 
         Simulator sim = null;
 
         try {
-            sim = new Simulator(1000,20000,100,3,50,50,50, 40);
+            sim = new Simulator(1000, 44999, 100, 3, 50, 50, 50, 45);
             //Strategy strategy = new PreemptiveLockdownAndStopSpread(sim,25, 2);
             //Strategy strategy=new BusinessAsUsual(sim);
+            //Strategy strategy = new ContactTracingMod(sim, 20);
         } catch (InvalidSimulationException e) {
             e.printStackTrace();
             System.out.println(e.getMessage());
             System.exit(0);
         }
         Simulator.Outcomes outcome;
-        System.out.println("DAY "+sim.getDay());
-        while ((outcome = sim.executeDay())== Simulator.Outcomes.NOTHING){
+        System.out.println("DAY " + sim.getDay());
+        while ((outcome = sim.executeDay()) == Simulator.Outcomes.NOTHING) {
             System.out.println();
-            System.out.println("DAY "+sim.getDay());
-            int black   = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLACK).count();
-            int green   = (int) sim.population.stream().filter(person -> person.getColor() == Color.GREEN).count();
-            int yellow  = (int) sim.population.stream().filter(person -> person.getColor() == Color.YELLOW).count();
-            int red     = (int) sim.population.stream().filter(person -> person.getColor() == Color.RED).count();
-            int blue    = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLUE).count();
+            System.out.println("DAY " + sim.getDay());
+            int black = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLACK).count();
+            int green = (int) sim.population.stream().filter(person -> person.getColor() == Color.GREEN).count();
+            int yellow = (int) sim.population.stream().filter(person -> person.getColor() == Color.YELLOW).count();
+            int red = (int) sim.population.stream().filter(person -> person.getColor() == Color.RED).count();
+            int blue = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLUE).count();
 
-            System.out.println("green : "+green );
-            System.out.println("yellow: "+yellow);
-            System.out.println("red   : "+red   );
-            System.out.println("blue  : "+blue  );
-            System.out.println("black : "+black );
-            System.out.println("resources : "+sim.getResources());
+            System.out.println("green : " + green);
+            System.out.println("yellow: " + yellow);
+            System.out.println("red   : " + red);
+            System.out.println("blue  : " + blue);
+            System.out.println("black : " + black);
+            System.out.println("resources : " + sim.getResources());
+            System.out.println("R0 factor: " + sim.r0);
         }
         System.out.println("\nFinal report:");
-        System.out.println(sim.getDay() +"d "+ outcome.toString());
+        System.out.println(sim.getDay() + "d " + outcome.toString());
 
-        int black   = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLACK).count();
-        int green   = (int) sim.population.stream().filter(person -> person.getColor() == Color.GREEN).count();
-        int yellow  = (int) sim.population.stream().filter(person -> person.getColor() == Color.YELLOW).count();
-        int red     = (int) sim.population.stream().filter(person -> person.getColor() == Color.RED).count();
-        int blue    = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLUE).count();
-        //int R0 = (sim.averageEncountersPerDay * sim.healDay * sim.infectionRate)/100;
-        // NOTA: non ricalcolare R0! è già calcolato in sim.R0, ed è anche calcolato con il Vd, non con il V
-        //TODO: dear Cencia, is this R0 useful?? <= <3
+        int black = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLACK).count();
+        int green = (int) sim.population.stream().filter(person -> person.getColor() == Color.GREEN).count();
+        int yellow = (int) sim.population.stream().filter(person -> person.getColor() == Color.YELLOW).count();
+        int red = (int) sim.population.stream().filter(person -> person.getColor() == Color.RED).count();
+        int blue = (int) sim.population.stream().filter(person -> person.getColor() == Color.BLUE).count();
 
-        System.out.println("green : "+green );
-        System.out.println("yellow: "+yellow);
-        System.out.println("red   : "+red   );
-        System.out.println("blue  : "+blue  );
-        System.out.println("black : "+black );
-        System.out.println("resources : "+sim.getResources());
-//        System.out.println("Fattore R0: " + R0);
+        System.out.println("green : " + green);
+        System.out.println("yellow: " + yellow);
+        System.out.println("red   : " + red);
+        System.out.println("blue  : " + blue);
+        System.out.println("black : " + black);
+        System.out.println("resources : " + sim.getResources());
+        System.out.println("R0 factor: " + sim.r0);
 
         long end = System.currentTimeMillis();
-        long time=(end-start);
+        long time = (end - start);
         System.out.println();
         System.out.println(time + " millisec");
     }
