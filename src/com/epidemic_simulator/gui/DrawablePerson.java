dfc93189e814f59151115c42d30a27a296dcdb37
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-05-09 07:29:35 +0200

    UI first WIP version it does simulate all, but it's pretty chaotic

--- src/com/epidemic_simulator/gui/DrawablePerson.java
+++ src/com/epidemic_simulator/gui/DrawablePerson.java
@@ -4,15 +4,24 @@ import com.epidemic_simulator.Person;
 import dev.federicocapece.jdaze.Engine;
 import dev.federicocapece.jdaze.GameObject;
 import dev.federicocapece.jdaze.Vector;
-import dev.federicocapece.jdaze.collider.Collider;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class DrawablePerson extends GameObject {
+    private static final Random rand = new Random();
+
+    private final int SIZE = 10;
+    private final int SPEED = 400+rand.nextInt(60);
+    private final int POSITION_PRECISION = 2;
+
+    protected Boolean moving = false;
+
     private final Vector startingPosition;
     private Person innerPerson;
 
-    private DrawablePerson target = null;
+    protected ArrayList<DrawablePerson> target;
 
     public DrawablePerson(Person innerPerson){
         this(innerPerson,0,0);
@@ -20,32 +29,53 @@ public class DrawablePerson extends GameObject {
 
     public DrawablePerson(Person innerPerson, float x, float y) {
         super(x,y);
+        target = new ArrayList<>();
         this.innerPerson = innerPerson;
-        startingPosition = position;
+        startingPosition = new Vector(position);
     }
 
 
     @Override
     protected void update() {
-        if(target != null){
-            //(target.position-position).normalized * speed * deltaTime
-            move(target.position.sub(this.position).normalize().multiply(10*Engine.deltaTime));
-        }
-        //position.sumUpdate(new Vector(10,10).multiply(Engine.deltaTime));
-    }
+        synchronized (moving){
+            moving = true;
+            Vector offset, movement;
+            if(target.size()>0){
+                //if my target is faster than me i'll just wait
+                //if(target.get(0).SPEED >= SPEED && target.get(0).target.size()>0) return;
 
-    @Override
-    protected void onCollisionEnter(Collider collider) {
-        if(collider.gameObject == target){
-            target = null;
-            extrapolate(collider);
+                //(target.position-position).normalized * deltaTime
+                offset = target.get(0).position.sub(this.position);
+                movement = offset.normalized().multiplyUpdate(SPEED * Engine.deltaTime);
+                if (movement.magnitude() < offset.magnitude()){
+                    //System.out.println("MOVING: "+this+" to "+target.get(0)+"MOVE:"+ movement);
+                    position.sumUpdate(movement);
+                }else{
+                    //i'm really close to the other person, i'm gonna consider that i've met him.
+                    target.remove(0);
+                }
+            }else if((offset = startingPosition.sub(position)).magnitude() > POSITION_PRECISION){
+
+                movement = startingPosition.sub(this.position).normalize().multiplyUpdate(SPEED * Engine.deltaTime);
+                if (movement.magnitude() < offset.magnitude()){
+                    //System.out.println("GOING BACK: "+position+" to "+startingPosition+"MOVE:"+ movement);
+                    position.sumUpdate(movement);
+                }else{
+                    position.set(startingPosition);
+                    moving = false;
+                }
+            }else{
+                moving = false;
+            }
+            //position.sumUpdate(new Vector(10,10).multiply(Engine.deltaTime));
         }
+
     }
 
     @Override
     protected void draw(Graphics graphics, int x, int y, float scale) {
         graphics.setColor(innerPerson.getColor());
-        int scaledSize = (int)(10*scale);
+        int scaledSize = (int)(SIZE*scale);
         int halfScaledSize = scaledSize/2;
         graphics.fillOval(x-halfScaledSize,y-halfScaledSize,scaledSize,scaledSize);
     }
--- src/com/epidemic_simulator/gui/SimulatorGUI.java
+++ src/com/epidemic_simulator/gui/SimulatorGUI.java
@@ -2,70 +2,134 @@ package com.epidemic_simulator.gui;
 
 import com.epidemic_simulator.Person;
 import com.epidemic_simulator.Simulator;
+import com.epidemic_simulator.SimulatorCallBack;
 import dev.federicocapece.jdaze.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 public class SimulatorGUI extends JFrame {
-    
-        Simulator simulator;
-    
-        public SimulatorGUI(Simulator simulator) {
-            this.simulator = simulator;
 
-            //creating the Frame
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            setSize((int)(screenSize.width*.95),(int)(screenSize.height*.9));
-    
-            setResizable(false);
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
-            JPanel contentPane = new JPanel(new BorderLayout());
-            setContentPane(contentPane);
-    
-            contentPane.add(Engine.renderer, BorderLayout.CENTER);
-    
-            //forcing component draw so i can get the canvas size
-            setVisible(true);
-            revalidate();
-            repaint();
+    Simulator simulator;
+    private HashMap<Person, DrawablePerson> drawablePersons;
+    private HashMap<DrawablePerson, Boolean> movingPersons;
 
-            //Starting the graphic engine
-            Engine.start(100);
+    public SimulatorGUI(Simulator simulator) throws NoSuchMethodException {
+        this.simulator = simulator;
 
-            //calculating the best rows and column configuration for the number of Persons that i have
-            int nPersons = 10000;
+        //creating the Frame
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize((int)(screenSize.width*.95),(int)(screenSize.height*.9));
 
-            int width = Engine.renderer.getWidth();
-            int height = Engine.renderer.getHeight();
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-            int nx = (int)Math.sqrt(((float)nPersons)*width/height);
-            int ny = (int)Math.sqrt(((float)nPersons)*height/width)+1;
+        JPanel contentPane = new JPanel(new BorderLayout());
+        setContentPane(contentPane);
 
-            //creating the persons
-            int created = 0;
-            creationLoop:
-            for (int y = 0; y < ny; y++){
-                for (int x = 0; x < nx; x++){
-                    created++;
-                    new DrawablePerson(new Person(), x*20, y*20);
-                    if (created == nPersons) break creationLoop;
-                }
+        contentPane.add(Engine.renderer, BorderLayout.CENTER);
+
+        //forcing component draw so i can get the canvas size
+        setVisible(true);
+        revalidate();
+        repaint();
+
+        //Starting the graphic engine
+        Engine.start();
+
+        //calculating the best rows and column configuration for the canvas rateo and the number of Persons that i have
+        int nPersons = simulator.population.size();
+
+        int width = Engine.renderer.getWidth();
+        int height = Engine.renderer.getHeight();
+
+        int nx = (int)Math.sqrt(((float)nPersons)*width/height);
+        int ny = (int)Math.sqrt(((float)nPersons)*height/width)+1;
+
+        //calculating the world size
+        int worldX = nx * 20;
+        int worldY = ny * 20;
+
+        //creating the persons
+        drawablePersons = new HashMap<>();
+        int created = 0;
+        creationLoop:
+        for (int y = 0; y < worldY; y+=20){
+            for (int x = 0; x < worldX; x+=20){
+                Person person = simulator.population.get(created);
+                drawablePersons.put(person, new DrawablePerson(person, x, y));
+                if (++created == nPersons) break creationLoop;
             }
-
-            //creating the camera movement script
-            new CameraMove(200);
-            //centering the camera
-            CameraMove.setPosition(nx*10, ny*10);
-
         }
 
+        //creating the camera movement script
+        CameraMove cameraScript = new CameraMove(200);
+        //centering the camera
+        cameraScript.setPosition(worldX/2f, worldY/2f);
+        //setting the camera bounds
+        cameraScript.setBound(0,0,worldX,worldY);
 
-        public static void main(String[] args) {
-            //fake main that just start a Setting window and call the start button
-            SimulatorSettings settings = new SimulatorSettings();
-            settings.startButtonListener.actionPerformed(null);
-        }
-    
+        //calculating the maxCameraZoom
+        cameraScript.setScales(.2f,30f);
+
+        //setting manual ideal zoom
+        Engine.camera.setScale((float)width/(worldX+40));
+
+        //linking simulator to GUI
+        simulator.callBacks.add(simulatorEventListener);
+
+        //starting simulator
+        simulator.executeDay();
+
     }
+
+    public static void main(String[] args) {
+        //fake main that just start a Setting window and call the start button
+        SimulatorSettings settings = new SimulatorSettings();
+        settings.startButtonListener.actionPerformed(null);
+    }
+
+    private final SimulatorCallBack simulatorEventListener = new SimulatorCallBack() {
+        int count = 0;
+        @Override
+        public void personHasSymptoms(Person person) {}
+
+        @Override
+        public void personClean(Person person) {}
+
+        @Override
+        public void registerEncounter(Person person1, Person person2) {
+            //if(count++<2)
+                drawablePersons.get(person1).target.add(drawablePersons.get(person2));
+        }
+
+        @Override
+        public void afterExecuteDay(Simulator.Outcome outcome) {
+            if(outcome != Simulator.Outcome.NOTHING){
+                System.out.println("OUTCOME: " + outcome);
+                return;
+            }
+            //TODO: WAIT TILL EVERY DRAWINGPERSON IS NOT MOVING, THEN CALL EXECUTEDAY
+            boolean moving = true;
+            whileLabel:
+            do{
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                moving = false;
+                for(DrawablePerson person : drawablePersons.values()){
+                    if(person.moving){
+                        moving = true;
+                        break;
+                    }
+                }
+            }while (moving);
+            simulator.executeDay();
+        }
+
+    };
+
+}
