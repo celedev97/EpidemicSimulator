2ba2e10563bb610e6053f2294e2fde7695afba84
Paoletto123 <luciano.1905339@studenti.uniroma1.it>
2020-05-16 15:49:18 +0300

    Added new simple Strategy for the block of the population on the first Red.

---
+++ src/strategies/StopEpidemyOnFirstRed.java
@@ -0,0 +1,43 @@
+package strategies;
+
+import com.epidemic_simulator.Person;
+import com.epidemic_simulator.Simulator;
+import com.epidemic_simulator.Strategy;
+
+public class StopEpidemyOnFirstRed extends Strategy {
+    private int sintomatici=0;
+    private int controllo=0;
+    private int data_check = 0;
+
+    public StopEpidemyOnFirstRed(Simulator simulator){
+        super(simulator);
+    }
+
+    @Override
+    public void afterExecuteDay(Simulator.Outcome outcome) {
+        if (simulator.getFirstRed()) {
+            if(this.controllo==0){
+                data_check=simulator.getDay();
+                System.out.println("DATA: "+data_check+" CANINFECTDAY: "+simulator.canInfectDay+" SYMPTOMSMAXDAY: "+simulator.developSymptomsMaxDay);
+                System.out.println("BEGINING OF THE LOCKDOWN UNTIL: "+((data_check+simulator.canInfectDay+simulator.developSymptomsMaxDay))+" DAY!");
+                for (Person p:simulator.getAlivePopulation()) {
+                    p.setCanMove(false);
+                }
+                this.controllo++;
+            }
+            //System.out.println(this.controllo>0&&simulator.getDay()==(data_check+simulator.canInfectDay+simulator.developSymptomsMaxDay));
+            if(this.controllo>0&&simulator.getDay()==((data_check+simulator.canInfectDay+simulator.developSymptomsMaxDay))){
+                System.out.println("START OF THE CONTROLL!");
+                int count=0;
+                for (Person p:simulator.getAlivePopulation()){
+                    if(!simulator.testVirus(p)){
+                        count++;
+                        p.setCanMove(true);
+                    }
+                }
+                System.out.println(count+" persone rimesse in libertà!");
+                data_check=0;
+            }
+        }
+    }
+}
