6163946bd66dd007609b4c160d510dace085180a
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-05-19 04:26:46 +0200

    added a failsafe version of the AggressiveContactTracing

---
+++ src/strategies/ContactTracingFailSafe.java
@@ -0,0 +1,31 @@
+package strategies;
+
+import com.epidemic_simulator.Person;
+import com.epidemic_simulator.Simulator;
+
+public class ContactTracingFailSafe extends AggressiveContactTracing {
+    float resourcesMinimum;
+
+    public ContactTracingFailSafe(Simulator simulator, int resourcesPercentageForLockDown) {
+        super(simulator);
+        resourcesMinimum = originalResources / 100f * resourcesPercentageForLockDown;
+    }
+
+    @Override
+    public void afterExecuteDay(Simulator.Outcome outcome) {
+        super.afterExecuteDay(outcome);
+
+        if(simulator.getResources() <= resourcesMinimum){
+            System.out.println("TRIGGERED EMERGENCY LOCKDOWN!!!");
+            resourcesMinimum = 0;
+            simulator.getAlivePopulation().forEach(person -> quarantine(person, simulator.canInfectDay));
+        }
+    }
+
+    @Override
+    public void personHasSymptoms(Person person) {
+        super.personHasSymptoms(person);
+    }
+
+
+}
