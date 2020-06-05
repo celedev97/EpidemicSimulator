731c8c86a61bdcc0e43e93dbbf920c5211897e0b
Fede Capece <capece.1744999@studenti.uniroma1.it>
2020-06-05 09:08:59 +0200

    main for performance tests

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
 import com.epidemic_simulator.Simulator;
 import com.epidemic_simulator.exceptions.InvalidSimulationException;
+import org.reflections.vfs.SystemDir;
 import strategies.StopEpidemyOnFirstRed;
 
 public class Main {
 
     public static void main(String[] args) throws InvalidSimulationException {
-        boolean dio, cane;
-        dio = cane = true;
+        int simulators = 1000;
 
-        int simulatorCreated = 0;
+        long startTime = System.currentTimeMillis();
 
-        while(dio == cane){
-            simulatorCreated++;
+        for (int i = 0; i < simulators; i++) {
             //create simulator
             Simulator simulator = new Simulator(10000, 449999,5,.3, 50,50,50,45);
             //link strategy
             StopEpidemyOnFirstRed strategy = new StopEpidemyOnFirstRed(simulator);
-            System.out.println("CREATED "+simulatorCreated+"SIMULATORS");
 
             while(simulator.executeDay() == Simulator.Outcome.NOTHING){}
-
+            System.out.println(i);
         }
+
+        System.out.println("time per simulator: " + ((System.currentTimeMillis() - startTime)/simulators) + "ms");
     }
 
 }
